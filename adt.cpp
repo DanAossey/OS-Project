b06b66fd6d33520a6109db60803ccec455b0d77c
DanAossey <92216534+DanAossey@users.noreply.github.com>
2021-10-11 17:55:56 -0400

    Update adt.cpp
    Added stuff for the Queue as a Linked List, got it working on my device with strings and I believe have properly updated for the process info struct

--- adt.cpp
+++ adt.cpp
@@ -2,6 +2,10 @@
 // Coder: J Yong
 // Purpose: this can be deleted 
 #include "adt.h"
+#include<iostream>
+using namespace std;
+
+const int LASTNODE = 0;
 
 //id arr burst priority
 bool read_File (struct Queue q) {
@@ -19,4 +23,74 @@ bool read_File (struct Queue q) {
   }
   return true;
   inFile.close();
+}
+
+// Mutators
+
+Queue::Queue(void){
+  head = new Process;
+  head->next = LASTNODE;
+  cursor = head;
+  tail = head;
+}
+
+Queue::~Queue(void) {
+  
+  moveToFront();
+  while (remove()){
+  }
+  delete head;
+}
+
+void Queue::insert(const DataType& newItem) {
+  Process* temp;
+  temp = new Process;
+  temp->contents = newItem;
+  
+  temp->next = cursor->next;
+  cursor->next = temp;
+  
+  if (temp->next == 0){
+    tail = temp;
+  }
+}
+
+void Queue::remove(void) {
+  Process* temp;
+  temp = cursor->next;
+  
+  if (temp == 0) {
+    return false;
+  }
+  cursor->next = temp->next;
+  
+  delete temp;
+  return true;
+}
+
+bool Queue::move(void){
+  if(cursor->next == 0){
+    return false;
+  }
+  cursor = cursor->next;
+  
+  return true;
+}
+
+void Queue::moveToBack(void){
+  cursor = tail;
+}
+
+void Queue::moveToFront(void){
+  cursor = head;
+}
+// Accessors
+
+bool Queue::get(DataType& item) const{
+  if (cursor->next == 0){
+    return false;
+  }
+  
+  item =  cursor->next->data;
+  return true;
 }
